99691db3d2b22c8ae5c80fa809759835767611de
Aiat <isamidinovaiat@gmail.com>
2021-07-14 18:16:40 +0600

    Angela's Solution

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-var randomNumber1 = Math.random();
-randomNumber1 = Math.floor(randomNumber1 * 6 + 1);
-console.log(randomNumber1);
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumberSrc1 = "images/dice" + randomNumber1 + ".png";
+document.querySelectorAll("img")[0].setAttribute("src", randomNumberSrc1);
 
-var randomNumber2 = Math.random();
-randomNumber2 = Math.floor(randomNumber2 * 6 + 1);
-console.log(randomNumber2);
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomNumberSrc2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomNumberSrc2);
 
-document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
-document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
 
 var title = document.querySelector("h1");
 
